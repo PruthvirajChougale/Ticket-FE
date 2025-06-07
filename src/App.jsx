553369bc29cf967a47279187e92dc2d4701c2f08
Pruthviraj Chougale <pruthvirajchougale17@gmail.com>
2025-06-08 01:02:35 +0530

    testing2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import RegHeader from "./header";
 import Login from "./login";
 import Signup from "./signup";
 import RegFooter from "./footer";
-import Home from "./home";
+//import Home from "./home";
 import Flight from "./components/flight";
 import "./App.css";
 const { Header, Content, Footer } = Layout;
@@ -22,7 +22,7 @@ function App() {
 							<Route path="/" element={<Navigate to="/login" />} />
 							<Route path="/login" element={<Login />} />
 							<Route path="/signup" element={<Signup />} />
-							<Route path="/home" element={<Home />} />
+							{/* <Route path="/home" element={<Home />} /> */}
 							<Route path="/:path" element={<Flight />} />
 						</Routes>
 					</Content>
